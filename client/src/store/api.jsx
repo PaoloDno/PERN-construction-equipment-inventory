b5ac1a089e4ff8a4897bda57d302de68adc82774
PaoloDno <cabrerapaolodino@gmail.com>
2026-09-11 11:15:24 +0800

    refactor: env for frontend

--- client/src/store/api.jsx
+++ client/src/store/api.jsx
@@ -1,4 +1,4 @@
-const baseURL = import.meta.env.API_URL || "http://localhost:5000/api";
+const baseURL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
 
 export const api = async (
   endpoint,
